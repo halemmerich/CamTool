8f8e2b6661de980a25dc10b3976dfa5405a36fd7
Martin Boonk <martin@boonk.info>
2017-07-08 07:23:27 +0200

    Read and store a context wide property file

--- de.dieklaut.camtool/src/de/dieklaut/camtool/Constants.java
+++ de.dieklaut.camtool/src/de/dieklaut/camtool/Constants.java
@@ -1,7 +1,7 @@
 package de.dieklaut.camtool;
 
 public interface Constants {
-	public static final String AUTOMATION_FILE_NAME = ".automation";
+	public static final String AUTOMATION_FILE_NAME = ".camtool";
 	public static final String SORTED_FILE_NAME = ".sorted";
 	public static final String FOLDER_ORIGINAL = "original";
 	public static final String FOLDER_SORTED = "sorted";
@@ -11,4 +11,7 @@ public interface Constants {
 	public static final String FOLDER_RESULT_FULL = "result_full";
 	public static final String FOLDER_RESULT_WEB = "result_web";
 	public static final String FOLDER_RESULT_SOCIAL = "result_social";
+	public static final String PROPERTY_VERSION_FILE_FORMAT = "FileFormat";
+	public static final String CURRENT_VERSION_FILE_FORMAT = "1";
+	public static final String FILE_PROPERTIES = ".properties";
 }
--- de.dieklaut.camtool/src/de/dieklaut/camtool/Context.java
+++ de.dieklaut.camtool/src/de/dieklaut/camtool/Context.java
@@ -1,24 +1,49 @@
 package de.dieklaut.camtool;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.Properties;
 
-public class Context {
+public class Context implements AutoCloseable{
 	
 	private Path root;
+	private Properties properties;
 	
 	public Context(Path root) {
 		this.root = root;
 		if (!root.toFile().isDirectory()) {
 			throw new IllegalArgumentException("Give root path " + root + " is not a directory");
 		}
+		properties = new Properties();
+		
+		Path propertiesPath = root.resolve(Constants.FILE_PROPERTIES);
+		if (propertiesPath.toFile().exists()) {
+			InputStream inStream;
+			try {
+				inStream = Files.newInputStream(propertiesPath);
+				properties.load(inStream);
+			} catch (IOException e) {
+				Logger.log("The properties file could not be read", e);
+			}
+		}
 	}
 	
 	public boolean isInitialized() {
-		return Paths.get(root.toString(), ".automation").toFile().exists();
+		return root.resolve(Constants.AUTOMATION_FILE_NAME).toFile().exists();
+	}
+	
+	public int getFormatVersion() {
+		return Integer.parseInt(properties.getProperty(Constants.PROPERTY_VERSION_FILE_FORMAT, Constants.CURRENT_VERSION_FILE_FORMAT));
 	}
 	
 	public Path getRoot() {
 		return root;
+	}
+
+	@Override
+	public void close() throws Exception {
+		properties.store(Files.newOutputStream(root.resolve(Constants.FILE_PROPERTIES)), "Properties for the CamTool");
 	}
 }
