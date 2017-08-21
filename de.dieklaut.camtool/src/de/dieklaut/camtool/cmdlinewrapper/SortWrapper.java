236c13d384aea55a14fcbc3b5c18d1f63e650a8d
Martin Boonk <martin@boonk.info>
2017-08-21 15:45:30 +0200

    Allow using all options of Sort in the wrapper

--- de.dieklaut.camtool/src/de/dieklaut/camtool/cmdlinewrapper/SortWrapper.java
+++ de.dieklaut.camtool/src/de/dieklaut/camtool/cmdlinewrapper/SortWrapper.java
@@ -12,10 +12,23 @@ public class SortWrapper extends AbstractWrapper {
 
 	private static final String OPT_NAME_SHORT = "n";
 	private static final String OPT_NAME = "name";
+	private static final String OPT_DETECT_BRACKETED_SHORT = "b";
+	private static final String OPT_DETECT_SERIES_SHORT = "s";
+	private static final String OPT_DETECT_MOVE_ALL_GROUPS_SHORT = "a";
+	private static final String OPT_DETECT_MOVE_COLLECTIONS_SHORT = "c";
+	private static final String OPT_DETECT_BRACKETED = "brackets";
+	private static final String OPT_DETECT_SERIES = "series";
+	private static final String OPT_DETECT_MOVE_ALL_GROUPS = "groups";
+	private static final String OPT_DETECT_MOVE_COLLECTIONS = "collections";
 
 	@Override
 	public Options getOptions() {
-		return new Options().addOption(Option.builder(OPT_NAME_SHORT).longOpt(OPT_NAME).desc("Sets the name for the sorting").build());
+		Options options = new Options().addOption(Option.builder(OPT_NAME_SHORT).longOpt(OPT_NAME).desc("Sets the name for the sorting").build());
+		options.addOption(Option.builder(OPT_DETECT_BRACKETED_SHORT).longOpt(OPT_DETECT_BRACKETED).desc("Detect bracketed shots and put them into a collection").build());
+		options.addOption(Option.builder(OPT_DETECT_SERIES_SHORT).longOpt(OPT_DETECT_SERIES).desc("Detect series shots and put them into a collection").build());
+		options.addOption(Option.builder(OPT_DETECT_MOVE_ALL_GROUPS_SHORT).longOpt(OPT_DETECT_MOVE_ALL_GROUPS).desc("Move all detected groups into their own folder").build());
+		options.addOption(Option.builder(OPT_DETECT_MOVE_COLLECTIONS_SHORT).longOpt(OPT_DETECT_MOVE_COLLECTIONS).desc("Move all detected collections into their own folder").build());
+		return options;
 	}
 
 	@Override
@@ -23,6 +36,18 @@ public class SortWrapper extends AbstractWrapper {
 		Sort sort = new Sort();
 		if (cmdLine.hasOption(OPT_NAME)) {
 			sort.setName(cmdLine.getOptionValue(OPT_NAME));
+		}
+		if (cmdLine.hasOption(OPT_DETECT_BRACKETED)) {
+			sort.setDetectBracketedShots(true);
+		}
+		if (cmdLine.hasOption(OPT_DETECT_SERIES)) {
+			sort.setDetectSeries(true);
+		}
+		if (cmdLine.hasOption(OPT_DETECT_MOVE_ALL_GROUPS)) {
+			sort.setMoveAllGroupsToFolder(true);
+		}
+		if (cmdLine.hasOption(OPT_DETECT_MOVE_COLLECTIONS)) {
+			sort.setMoveCollectionsToFolder(true);
 		}
 		return sort;
 	}
