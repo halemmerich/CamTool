5a7d047900360511d4407cf556e50a1f9d3d50c2
Martin Boonk <martin@boonk.info>
2017-09-06 07:13:54 +0200

    Renames the result folder for a shorter total path

--- de.dieklaut.camtool/src/de/dieklaut/camtool/Constants.java
+++ de.dieklaut.camtool/src/de/dieklaut/camtool/Constants.java
@@ -8,9 +8,9 @@ public interface Constants {
 	public static final String FOLDER_RESULTS = "results";
 	public static final String FOLDER_UNUSED = "unused";
 	public static final String FOLDER_TIMELINE = "timeline";
-	public static final String FOLDER_RESULT_FULL = "result_full";
-	public static final String FOLDER_RESULT_WEB = "result_web";
-	public static final String FOLDER_RESULT_SOCIAL = "result_social";
+	public static final String FOLDER_RESULT_FULL = "full";
+	public static final String FOLDER_RESULT_WEB = "web";
+	public static final String FOLDER_RESULT_SOCIAL = "social";
 	public static final String PROPERTY_VERSION_FILE_FORMAT = "FileFormat";
 	public static final String CURRENT_VERSION_FILE_FORMAT = "1";
 	public static final String FILE_PROPERTIES = ".properties";
