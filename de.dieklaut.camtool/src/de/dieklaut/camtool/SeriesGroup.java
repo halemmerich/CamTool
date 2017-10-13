ab621b012621ae0cfe38dbb5f8cdd836e4e14b9d
Martin Boonk <martin@boonk.info>
2017-10-13 17:58:20 +0200

    Remove empty implementation

--- de.dieklaut.camtool/src/de/dieklaut/camtool/SeriesGroup.java
+++ de.dieklaut.camtool/src/de/dieklaut/camtool/SeriesGroup.java
@@ -3,8 +3,6 @@ package de.dieklaut.camtool;
 import java.nio.file.Path;
 import java.util.Collection;
 
-import de.dieklaut.camtool.renderjob.RenderJob;
-
 public class SeriesGroup extends MultiGroup {
 
 	public SeriesGroup(Collection<Group> groups) {
@@ -13,12 +11,6 @@ public class SeriesGroup extends MultiGroup {
 
 	public SeriesGroup(Collection<Group> groups, Path markerFile) {
 		super(groups, markerFile);
-	}
-
-	@Override
-	public RenderJob getRenderJob() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
