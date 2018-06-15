a88bbc1885aa76da2bbf57c76a9eef8172a9ca9b
Martin Boonk <martin@boonk.info>
2018-06-15 18:22:59 +0200

    Test combining of series

---
+++ de.dieklaut.camtool.test/src/de/dieklaut/camtool/SortingHelperTest.java
@@ -0,0 +1,36 @@
+package de.dieklaut.camtool;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.LinkedList;
+
+import org.junit.Test;
+
+public class SortingHelperTest extends FileBasedTest{
+	@Test
+	public void testCombineSeries() throws IOException {
+		Collection<Group> groups = new LinkedList<>();
+		
+		Path source = TestFileHelper.getTestResource("series/series_09.ARW");
+		groups.add(new SingleGroup(Files.copy(source, getTestFolder().resolve("series_09.ARW"))));
+		
+		source = TestFileHelper.getTestResource("series/series_07.ARW");
+		groups.add(new SingleGroup(Files.copy(source, getTestFolder().resolve("series_07.ARW"))));
+
+		source = TestFileHelper.getTestResource("series/series_06.ARW");
+		groups.add(new SingleGroup(Files.copy(source, getTestFolder().resolve("series_06.ARW"))));
+		
+		source = TestFileHelper.getTestResource("series/series_08.ARW");
+		groups.add(new SingleGroup(Files.copy(source, getTestFolder().resolve("series_08.ARW"))));
+				
+		SortingHelper.combineSeries(groups);
+		
+		assertEquals(1, groups.size());
+		assertEquals(4, ((MultiGroup)groups.stream().findFirst().get()).getGroups().size());
+		
+	}
+}
