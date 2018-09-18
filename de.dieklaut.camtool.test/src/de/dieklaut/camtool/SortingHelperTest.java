21ea552c9100a87e38d84dc8e53c8de9cc48dd31
Martin Boonk <martin@boonk.info>
2018-09-18 23:38:04 +0200

    Test multiple series detection

--- de.dieklaut.camtool.test/src/de/dieklaut/camtool/SortingHelperTest.java
+++ de.dieklaut.camtool.test/src/de/dieklaut/camtool/SortingHelperTest.java
@@ -33,4 +33,35 @@ public class SortingHelperTest extends FileBasedTest{
 		assertEquals(4, ((MultiGroup)groups.stream().findFirst().get()).getGroups().size());
 		
 	}
+	
+	@Test
+	public void testCombineMultipleSeries() throws IOException {
+		Collection<Group> groups = new LinkedList<>();
+		
+		Path source = TestFileHelper.getTestResource("series/series_09.ARW");
+		groups.add(new SingleGroup(Files.copy(source, getTestFolder().resolve("series_09.ARW"))));
+		
+		source = TestFileHelper.getTestResource("series/series_07.ARW");
+		groups.add(new SingleGroup(Files.copy(source, getTestFolder().resolve("series_07.ARW"))));
+
+		source = TestFileHelper.getTestResource("series/series_06.ARW");
+		groups.add(new SingleGroup(Files.copy(source, getTestFolder().resolve("series_06.ARW"))));
+		
+		source = TestFileHelper.getTestResource("series/series_08.ARW");
+		groups.add(new SingleGroup(Files.copy(source, getTestFolder().resolve("series_08.ARW"))));
+		
+		source = TestFileHelper.getTestResource("stack/stack_01.jpg");
+		groups.add(new SingleGroup(Files.copy(source, getTestFolder().resolve("stack_01.jpg"))));
+		
+		source = TestFileHelper.getTestResource("stack/stack_02.jpg");
+		groups.add(new SingleGroup(Files.copy(source, getTestFolder().resolve("stack_02.jpg"))));
+		
+		source = TestFileHelper.getTestResource("stack/stack_03.jpg");
+		groups.add(new SingleGroup(Files.copy(source, getTestFolder().resolve("stack_03.jpg"))));
+				
+		SortingHelper.combineSeries(groups, 2);
+		
+		assertEquals(2, groups.size());
+		
+	}
 }
