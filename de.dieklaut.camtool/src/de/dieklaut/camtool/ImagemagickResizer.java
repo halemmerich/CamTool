c5d4fd640932ba1d5fb50c1da96cadf79366a717
Martin Boonk <martin@boonk.info>
2019-08-05 19:27:16 +0200

    Apply exif orientation before stripping exif data

--- de.dieklaut.camtool/src/de/dieklaut/camtool/ImagemagickResizer.java
+++ de.dieklaut.camtool/src/de/dieklaut/camtool/ImagemagickResizer.java
@@ -13,6 +13,7 @@ public class ImagemagickResizer implements ImageResizer {
 	public boolean resize(int maxDimension, Path sourceFile, Path destinationFile, int qualityPercentage) {
 		CommandLine commandline = new CommandLine("convert");
 		commandline.addArgument(sourceFile.toAbsolutePath().toString(), false);
+		commandline.addArgument("-auto-orient");
 		commandline.addArgument("-strip");
 		commandline.addArgument("-quality");
 		commandline.addArgument(Integer.toString(qualityPercentage), false);
